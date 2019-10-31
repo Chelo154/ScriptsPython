7dffef1bd6cd89aa03bf0466f3bcace2333c7ad1
chelo154 <chelo154@gmail.com>
2019-10-31 01:27:52 -0300

    ConversorAscii ahora soporta multiples expresiones

--- ConversorAscii.py
+++ ConversorAscii.py
@@ -1,6 +1,20 @@
 import sys
+maximo = len(sys.argv)
 i = 0
-expresion = sys.argv[1]
-valorAscii = ''.join(str(ord(c)) for c in expresion)
-print("Valor en Ascii: "+valorAscii)
-
+print("\n")
+print("**************************************")
+print("*  CONVERSOR DE EXPRESIONES A ASCII  *")
+print("**************************************")
+print("by @chelo154")
+print("--------------------------------------")
+print("\n")
+while(i < maximo):
+    if(i != 0):
+        print("Expresion "+str(i)+": ")      
+        expresion = sys.argv[i]
+        valorAscii = ''.join(str(ord(c)) for c in expresion)
+        print("Valor Original: "+expresion)
+        print("Valor en Ascii: "+valorAscii)
+        print("")
+    i=i+1
+print("Expresiones Convertidas Exitosamente :D ")
